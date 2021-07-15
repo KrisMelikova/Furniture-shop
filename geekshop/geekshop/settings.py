336e3bc1966e3647106056663611dc4bc6a27f84
Kris <kristina@melikova.ru>
2021-07-15 11:50:45 +0300

    change settings

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -168,14 +168,15 @@ if DEBUG:
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 '''
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-
+'''
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-'''
+
 
 AUTH_USER_MODEL = 'authapp.ShopUser'
 
