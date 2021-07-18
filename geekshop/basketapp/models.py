c3c05232a4b093b5f6b13031a6a489bb58d0995a
Kris <kristina@melikova.ru>
2021-07-18 09:53:20 +0300

    add @cached_property

--- geekshop/basketapp/models.py
+++ geekshop/basketapp/models.py
@@ -1,3 +1,5 @@
+from functools import cached_property
+
 from django.db import models
 from django.conf import settings
 from mainapp.models import Product
@@ -15,7 +17,7 @@ class BasketQuerySet(models.QuerySet):  # для работы с queryset тип
 class Basket(models.Model):
     objects = BasketQuerySet.as_manager()
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='basket')
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
     add_datetime = models.DateTimeField(verbose_name='время добавления', auto_now_add=True)
@@ -26,21 +28,25 @@ class Basket(models.Model):
     
     product_cost = property(_get_product_cost)
 
-    def _get_total_quantity(self):
-        "return total quantity for user"
-        _items = Basket.objects.filter(user=self.user)
+    @cached_property
+    def get_items_cached(self):
+        return self.user.basket.select_related()
+
+    def get_total_quantity(self):
+        # _items = Basket.objects.filter(user=self.user)
+        _items = self.get_items_cached
         _totalquantity = sum(list(map(lambda x: x.quantity, _items)))
         return _totalquantity
         
-    total_quantity = property(_get_total_quantity)
+    total_quantity = property(get_total_quantity)
 
-    def _get_total_cost(self):
-        "return total cost for user"
-        _items = Basket.objects.filter(user=self.user)
+    def get_total_cost(self):
+        # _items = Basket.objects.filter(user=self.user)
+        _items = self.get_items_cached
         _totalcost = sum(list(map(lambda x: x.product_cost, _items)))
         return _totalcost
         
-    total_cost = property(_get_total_cost)
+    total_cost = property(get_total_cost)
 
     @staticmethod
     def get_items(user):
